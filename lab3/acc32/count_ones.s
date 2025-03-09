4a0dde682d6f03c4123218c6a65d7583eae0662d
2BuRy1 <kot514877@gmail.com>
2025-03-09 21:12:10 +0300

    fixed code, all tests are passed✅

--- lab3/acc32/count_ones.s
+++ lab3/acc32/count_ones.s
@@ -3,9 +3,10 @@
 input_addr: .word 0x80
 output_addr: .word 0x84
 number: .word 0
-mask: .word 0x1
+masForSign: .word 0x1
 counter: .word 0x0
 amount: .word 0x1
+maskForNeg: .word 0x7fffffff
 .text
 
 
@@ -15,8 +16,9 @@ _start:
 
 loop:
     load number
+    ble ifneg
     beqz end
-    and mask
+    and masForSign
     bnez incr
     load number
     shiftr amount
@@ -31,6 +33,14 @@ incr:
     store number
     jmp loop
 
+ifneg:
+    load number
+    and maskForNeg
+    store number
+    load counter
+    add amount
+    store counter
+    jmp loop
 end:
     load counter
     store_ind output_addr
